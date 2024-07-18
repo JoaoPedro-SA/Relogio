99fe1b991b7567340d571d09a0a44e36c3a1e7b1
JoaopedroSA-cmd <antunesjoaopedro3@gmail.com>
2024-07-18 13:14:34 -0300

    adicionar modo cronometro

---
+++ motor/event.js
@@ -0,0 +1,76 @@
+let h = 0;
+let m = 0;
+let s = 0;
+let sentido;
+let stop;
+let interval;
+relogio.textContent = `${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`;
+
+// função para iniciar e para o relogio
+Relogio = () => {
+  
+     if (s < 59 ){
+          s = s + 1;    
+          relogio.textContent = `${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`; 
+             if (m < 59 && s == 59){
+               m++;
+               s = 0;              
+              if (h < 24 && m == 59){
+                   h++;
+                   m = 0;
+               
+           }
+          }
+     }
+  return h,m,s;
+}
+
+function loop(i,i2){
+     if (i === true && i2 === 1){
+          interval = setInterval (() => {
+               Relogio();
+               return false
+               
+          }, 1000);  
+     }
+     else{
+          return false, i2 = -1;
+     }
+     
+}
+
+// BOTOES 
+i = 0;
+iniciar.addEventListener('click', () => {
+    relogio.style.color = corL.value;
+    stop = true;
+    if (stop === true){
+     i++;
+    stop, i = loop(stop,i);  
+    }
+
+});
+
+resetar.addEventListener('click', () => {
+     clearInterval(interval);
+     stop = false;
+     relogio.style.color = corL.value;
+     s = 0;
+     m = 0;
+     h = 0;
+     i = 0;
+     relogio.textContent = `${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`;
+});
+
+pausar.addEventListener('click', () => {
+     stop = true;
+     clearInterval(interval);
+     relogio.style.color = 'red';   
+     loop(false);
+     i = 0;
+
+});
+
+
+
+
--- motor/motor.js
+++ motor/motor.js
@@ -1,38 +1,38 @@
+const result = document.querySelector('.data');
 function updateDate() {
-     // Seleciona o elemento HTML com a classe 'data'
-     const result = document.querySelector('.data');
-     
-     // Obtém a data atual
-     const dataAtual = new Date();
-   
-     // Converte a data em uma string formatada sem o fuso horário
-     const dataFormatada = dataAtual.toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo", hour12:"dataAtual" ,});
-     
+  // Seleciona o elemento HTML com a classe 'data'
 
-      const data2 = dataFormatada.toString();
-    /* essa opção 👇⌚ para formata o data
-     dataAtual.toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
-     */
-     // Define o conteúdo do elemento HTML 'result' como a data formatada
-     result.textContent = data2;
-   
-     // Agende a próxima atualização eficientemente usando 'requestAnimationFrame'
-     requestAnimationFrame(updateDate);
-   }
-   
-   // Chamada inicial da função 'updateDate'
-   requestAnimationFrame(updateDate);
+  // Obtém a data atual
+  const dataAtual = new Date();
+
+  // Converte a data em uma string formatada sem o fuso horário
+  const dataFormatada = dataAtual.toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo", hour12: "dataAtual", });
 
 
-   const objeto = document.querySelector('.corpo');
+  const data2 = dataFormatada.toString();
+  /* essa opção 👇⌚ para formata o data
+   dataAtual.toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
+   */
+  // Define o conteúdo do elemento HTML 'result' como a data formatada
+  result.textContent = data2;
+
+  // Agende a próxima atualização eficientemente usando 'requestAnimationFrame'
+  requestAnimationFrame(updateDate);
+}
+
+// Chamada inicial da função 'updateDate'
+requestAnimationFrame(updateDate);
+
+
+const objeto = document.querySelector('.corpo');
 
 objeto.addEventListener('click', () => {
   objeto.classList.add('animando');
 
   setTimeout(() => {
     objeto.classList.remove('animando');
-  }, 2000);   
-});     
+  }, 2000);
+});
 const button = document.querySelector('.button');
 const bord = document.querySelector('.body');
 const cor = document.querySelector('.cor');
@@ -43,15 +43,46 @@ button.addEventListener('click', () => {
   console.log(cor.value);
   bord.style.backgroundColor = cor.value;
   data_cor.style.color = corL.value;
+  relogio.style.color = corL.value;
   data_cor.style.textShadow = `0px 60px 10px ${corL.value}`;
 })
 
+const iniciar = document.getElementById("iniciar");
+const pausar = document.getElementById("pausar");
+const resetar = document.getElementById("zerar");
 
 
+function modelo_relogio() {
 
+  const button2 = document.querySelector('.button2');
 
+  button2.addEventListener('click', () => {
+    iniciar.style.display = 'none';
+    pausar.style.display = 'none';
+    resetar.style.display = 'none';
+    result.style.display = '';
+    relogio.style.display = 'none';
+  })
 
+}
 
+modelo_relogio();
 
+const relogio = document.querySelector('#relogio');
 
+function modelo_cronometro() {
+  const button3 = document.querySelector('.button3');
+
+  button3.addEventListener('click', () => {
+    iniciar.style.display = '';
+    pausar.style.display = '';
+    resetar.style.display = '';
+    result.style.display = 'none';
+    relogio.style.display = '';
+
+  })
+
+}
+
+modelo_cronometro();
 
